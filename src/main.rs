578194a498640f03f7f3ec2ff3ec23cceee5c564
fofful <32476899+fofful@users.noreply.github.com>
2022-10-02 20:30:17 +0300

    Update main.rs
    fixed first row printing

--- src/main.rs
+++ src/main.rs
@@ -8,12 +8,14 @@ fn main() {
     let j_max = args[4].parse::<i32>().unwrap();
     let mut max_num = i_max * j_max;
     let mut count = 0;
+
     while max_num > 0 {
         max_num = max_num / 10;
         count = count + 1;
     };
     let mut j_max_copy = j_max.clone();
     let mut j_count = 0;
+
     while j_max_copy > 0 {
         j_max_copy = j_max_copy / 10;
         j_count = j_count + 1;
@@ -22,40 +24,49 @@ fn main() {
     for j_c in 0..j_count{
         print!(" ");
     }
-    for i in i_min..i_max{
-        let num = i;
+
+    for j in j_min..j_max{
+        let num = j;
             let mut num_copy = num.clone();
             let mut head = 0;
+
             while num_copy > 0 {
                 num_copy= num_copy / 10;
                 head = head + 1;
             };
+
             for c in head..count+1{
                 print!(" ");
             }
             print!("{}", num);
     }
+
     println!("");
 
     for i in i_min..i_max{
         let mut i_curr_copy = i.clone();
         let mut i_count = 0;
+
         while i_curr_copy > 0 {
             i_curr_copy = i_curr_copy / 10;
             i_count = i_count + 1;
         };
+
         for j_c in i_count..j_count{
             print!(" ");
         }
         print!("{}", i);
+
         for j in j_min..j_max{
             let num = i * j;
             let mut num_copy = num.clone();
             let mut head = 0;
+
             while num_copy > 0 {
                 num_copy= num_copy / 10;
                 head = head + 1;
             };
+
             for c in head..count+1{
                 print!(" ");
             }
